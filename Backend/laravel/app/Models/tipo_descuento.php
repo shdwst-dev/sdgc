344580e38cf09e767791ad8eea7ee61d0786c037
Fernando <124049519@upq.edu.mx>
2026-03-04 23:43:03 -0600

    tipo_decuento modelo agregado

--- Backend/laravel/app/Models/tipo_descuento.php
+++ Backend/laravel/app/Models/tipo_descuento.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class tipo_descuento extends Model
 {
     //
+    protected $table = 'tipo_descuento';
+    protected $primaryKey = 'id_tipo_descuento';
+    protected $fillable = [
+        'nombre',
+    ];
+
+
 }
